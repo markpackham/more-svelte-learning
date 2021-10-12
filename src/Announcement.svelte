12c32d19355e48d0d05bb3cea87d2a7d8dc9b5f4
mark <markpackham1@gmail.com>
2021-10-12 11:46:34 +0100

    add conditional styling
    https://youtu.be/0I_B6o2RX5A?t=2677

--- src/Announcement.svelte
+++ src/Announcement.svelte
@@ -1,16 +1,29 @@
 <script>
+    let show = true;
 
+    const hideAlert = () =>{
+        show = false;
+    }
+
+let warning = true;
 </script>
 
-<div class="alert">
+{#if show}
+<div class="alert" class:warning={warning}>
 50% off all fruit!
+<button on:click={hideAlert}>Hide</button>
 </div>
+{/if}
 
 <style>
-.alert{
-    padding: 8px;
-    font-size: 18px;
+.alert {
+	padding: 8px;
+	font-size: 18px;
+	background-color: darkblue;
+	color: white;
+}
+
+.warning{
     background-color: darkred;
-    color: white;
 }
 </style>
