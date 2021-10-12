043630a30469bd9f32b619adfb083a52d36dc470
mark <markpackham1@gmail.com>
2021-10-12 12:38:35 +0100

    use on:click with div
    https://youtu.be/0I_B6o2RX5A?t=3160

--- src/App.svelte
+++ src/App.svelte
@@ -1,32 +1,20 @@
+
 <script>
-	import Button from "./Button.svelte"
+	import Button from "./Button.svelte";
+	const log = () => {
+		console.log("Button clicked")
+	}
 </script>
 
 <main>
-	<Button btnText="Overriden"/>
+	<Button on:click={log} />
+	<button on:click={log}>App button</button>
 </main>
 
 <style>
-	h5, p {
-		margin: 4px 0px;
-	}
-	.card {
-		min-width: 400px;
-		padding: 8px;
-		border-radius: 20px;
-		border: 2px solid gray;
-		margin-top: 8px;
-	}
 	main {
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-	}
-	h1 {
-		font-size: 72px;
-		text-align: center;
-	}
-	h3 {
-		font-size: 38px;
 	}
 </style>
