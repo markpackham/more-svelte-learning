53dd7fe53ac8210bd33c99a635257e9146c95cda
mark <markpackham1@gmail.com>
2021-10-12 10:01:30 +0100

    bind input value
    https://youtu.be/0I_B6o2RX5A?t=1016

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,40 @@
 <script>
-	let name = "World"
-</script>
+	let name = "";
+	let src = "favicon.png";
+	let count = 0;
+	const incrementCounter = () => {
+		count += 1;
+	}
 
+	const handleInput = (event) => {
+		name = event.target.value;
+	}
+
+	const resertName = () => {
+		name = "Unknown"
+	}
+</script>
 
 <main>
 	<h1>Hello {name}</h1>
+
+	<input type="text" bind:value={name}>
+	<button on:click={resertName}>Set name to unknown</button>
+
+	<img {src} alt="Svelte logo">
 </main>
 
 <style>
-	h1{
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+	}
+	h1 {
 		font-size: 72px;
 		text-align: center;
+	}
+	h3 {
+		font-size: 38px;
 	}
 </style>
